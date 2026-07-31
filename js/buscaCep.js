d0bd9a42beaf5c274b19c3829608ee188c13e191
Kaique Moura <kaique29082009@gmail.com>
2026-07-31 10:50:35 -0300

    Create buscaCep.js

---
+++ js/buscaCep.js
@@ -0,0 +1,14 @@
+//#region importações
+
+//#endregion
+
+//#region captura da api
+
+//#endregion
+
+//#region captura do input
+let buscaCepDisplay = document.querySelector("#displayCep");//area que vai mostrar os dados
+
+let inputUser = document.querySelector("#InputUser");
+let inputUser_value = inputUser.value; //captura o que foi digitado
+//#endregion

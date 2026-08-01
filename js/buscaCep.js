4b619005617219c9911900c7bb8b8b28e3749419
Kaique Moura <kaique29082009@gmail.com>
2026-07-31 22:51:11 -0300

    Update buscaCep.js

--- js/buscaCep.js
+++ js/buscaCep.js
@@ -8,7 +8,24 @@
 
 //#region captura do input
 let buscaCepDisplay = document.querySelector("#displayCep");//area que vai mostrar os dados
+let buscaCepSearch = document.querySelector(".buscaCep_search");//onde está as ferramentas de pesquisa
 
 let inputUser = document.querySelector("#InputUser");
 let inputUser_value = inputUser.value; //captura o que foi digitado
+
+let buscaCepBtn = document.querySelector("#searchEnviate");
+
+if(buscaCepDisplay){
+    buscaCepBtn.addEventListener('click', () => {
+        buscaCepBtn.innerHTML = "Apagar";
+        
+        buscaCepSearch.addEventListener('input', () => {
+            buscaCepBtn.innerHTML = "Enviar";
+        });
+    });
+}
+//#endregion
+
+//#region funções
+
 //#endregion

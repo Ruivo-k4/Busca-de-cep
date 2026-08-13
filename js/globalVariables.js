e6e923a45f89da17270170be665e811b3d51ff03
wesleysds77-blip <wesleysds77@gmail.com>
2026-08-13 14:21:06 -0300

    Update globalVariables.js

--- js/globalVariables.js
+++ js/globalVariables.js
@@ -1,7 +1,18 @@
 const historicoCeps = JSON.parse(localStorage.getItem('cepsBuscados')) || {};
+const historicoEstado = JSON.parse(localStorage.getItem('cepsBuscados')) || {};
+const historicoCidade = JSON.parse(localStorage.getItem('cepsBuscados')) || {};
+const historicoLogradouro = JSON.parse(localStorage.getItem('cepsBuscados')) || {};
 
-function config(newHisto){
+export function config(newHisto, newState, newCity, newPlace){
     historicoCeps = newHisto;
+    historicoEstado = newState;
+    historicoCidade = newCity;
+    historicoLogradouro = newPlace;
+
+    localStorage.setItem("historicoCeps", JSON.stringify(historicoCeps));
+    localStorage.setItem("historicoEstado", JSON.stringify(historicoEstado));
+    localStorage.setItem("historicoCidade", JSON.stringify(historicoCidade));
+    localStorage.setItem("historicoLogradouro", JSON.stringify(historicoLogradouro));
     
     
 }
